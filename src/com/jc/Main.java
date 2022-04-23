dc9bcf95635f38e0746d2e83af372135c2290d29
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2022-04-23 18:54:48 -0300

    refactor code

--- src/com/jc/Main.java
+++ src/com/jc/Main.java
@@ -17,19 +17,9 @@ public class Main {
         Node tree = parser.getTree();
         HashMap<String, String> symbolTable = parser.getSymbolTable();
         HashMap<String, String> tempTable = parser.getTempTable();
+        
         ThreeAddressCodeGenerator codeGenerator = new ThreeAddressCodeGenerator();
         List<Command> code = codeGenerator.generate(tree);
-
-        String layout = "%-5s %-6s %-6s %-6s %-6s\n";
-        System.out.format(layout, "label", "op", "right", "left", "dest");
-        for (Command command : code) {
-            System.out.format(layout, 
-                (command.label != null) ? command.label : "", 
-                command.op, 
-                command.left, 
-                command.right, 
-                command.result);
-        }
 
         Runner runner = new Runner(code, symbolTable, tempTable);
         runner.run();
